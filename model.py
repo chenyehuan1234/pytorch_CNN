fd41339ecfcc25b5fc995636c354cc585a3cb6b5
chenyehuan1234 <1744374759@qq.com>
2026-03-24 22:55:50 +0800

    1.02

--- model.py
+++ model.py
@@ -3,9 +3,11 @@ from torch import nn
 from torchsummary import summary
 
 
+
 class LeNet(nn.Module):
     def __init__(self):
         super(LeNet, self).__init__()
         self.c1 = nn.Conv2d(1, 6, 5,padding=2)
         self.sig = nn.Sigmoid()
         self.c2=nn.Conv2d(6, 16, 5,padding=2)
+
